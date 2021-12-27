7ac5ea6d7618f78d2da4c1dbbb5ef4714784ba40
y.pan <y.pan2005@icloud.com>
2021-12-27 14:31:14 +0800

    upgrade proto

--- grpc_service.go
+++ grpc_service.go
@@ -13,17 +13,24 @@ type GrpcSonyflakeService struct {
 }
 
 // NextID generate sequence
-func (rpcS *GrpcSonyflakeService) NextID(context.Context, *SonyFlakeRequest) (*SonyFlakeResponse, error) {
-	id, err := rpcS.Sf.NextID()
-	if err != nil {
-		return nil, err
+func (rpcS *GrpcSonyflakeService) NextID(ctx context.Context, req *SonyFlakeRequest) (*SonyFlakeResponse, error) {
+	var (
+		sequence uint64
+		err      error
+	)
+
+	for idx := 0; idx < int(req.GetNum()); idx++ {
+		sequence, err = rpcS.Sf.NextID()
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	idParts := Decompose(id)
+	idParts := Decompose(sequence)
 
 	return &SonyFlakeResponse{
-		ID:        idParts["id"],
-		Msb:       idParts["msg"],
+		Id:        idParts["id"],
+		Msb:       idParts["msb"],
 		Time:      idParts["time"],
 		Sequence:  idParts["sequence"],
 		MachineID: idParts["machine-id"],
